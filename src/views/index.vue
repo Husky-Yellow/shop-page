32b06adf371b7c7ab1d671eabee73fca3eda22f9
husky-yellow <473795376@qq.com>
2023-11-11 23:30:33 +0800

    fix: code

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,19 +1,14 @@
 <template funcitonal>
     <main class="index-box">
-        <h2>商品</h2>
+        <h2>商品列表</h2>
         <div class="container">
-            <div v-for="(item, index) in list" :key="index"  class="margin-t-20 fz-16">
+            <div v-for="(item, index) in list" :key="index" class="margin-t-20 fz-16">
                 <figure @click="open(item.key)">
-                    <img
-                        :src="item.img"
-                        loading="lazy"
-                        decoding="async"
-                        class="width-auto"
-                        />
+                    <img :src="item.img" loading="lazy" decoding="async"  />
                     <figcaption>
                         <p>
                             <span>{{ item.title }}</span>
-                            <span>查看详情 > </span>
+                            <span>查看详情 ➡️ </span>
                         </p>
                     </figcaption>
                 </figure>
@@ -52,6 +47,11 @@ export default {
 }
 </script>
 <style scoped>
+.index-box {
+    background: #f4f4f4;
+    min-height: 100vh;
+}
+
 .index-box>img {
     height: 120px;
     width: 100%;
@@ -61,63 +61,91 @@ export default {
     border-bottom: 1px solid #ccc;
     padding: 10px 8px;
 }
+
 .container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 10px;
-  padding: 10px 8px;
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    gap: 10px;
+    padding: 10px 8px;
 }
-.container >div{
+
+.container>div {
+    background-color: #fff;
+    height: 198px;
     overflow: hidden;
 }
-.container > div > figure {
+
+.container>div>figure {
     position: relative;
     overflow: hidden;
     z-index: 2;
 }
-.container > div > figure > figcaption {
+.container>div>figure img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.container>div>figure>figcaption {
     position: absolute;
     bottom: 0;
     left: 0;
-    transform: translate(12px, 60px);
+    transform: translate(8px, 36px);
     transition: all 0.2s ease-in-out;
     color: #ffffff;
-    display: flex;
+    display: inline-flex;
+    width: 80%;
+    padding: 10px;
 }
-.container >div:hover figcaption {
+
+.container>div> figure:hover figcaption {
     transform: translate(12px, -12px);
+    width: calc(100% - 24px);
 }
-.container >div:hover img {
+
+.container>div> figure:hover img {
     transition: 1s;
     transform: scale(1.1);
 }
-.container p{
+
+.container p {
     margin-top: 8px;
     display: flex;
     justify-content: space-between;
+    align-items: end;
+    width: 100%;
     /* display: none; */
 }
-.container p> span:nth-child(2) {
+
+.container p>span:nth-child(2) {
     opacity: .7;
-    font-size: 14px;
-    margin-left: auto;
+    font-size: 12px;
 }
 
 @media (max-width: 600px) {
-  .container {
-    grid-template-columns: 1fr;
-  }
-  .container > div > figure > figcaption {
-    position: relative;
-    transform: translate(0, 0);
-    color: black;
-    display: block;
-  }
-  .container >div:hover figcaption {
-    transform: none;
+    .container {
+        grid-template-columns: 1fr;
     }
-.container >div:hover img {
-    transform: none;
-}
-}
-</style>
+    .container>div {
+        height: 208px;
+    }
+
+    .container>div>figure>figcaption {
+        position: relative;
+        transform: translate(0, 0);
+        color: black;
+        display: block;
+        width: 100%;
+    }
+    .container p {
+        margin-top: 0;
+    }
+
+    .container>div > figure:hover figcaption {
+        transform: none;
+    }
+
+    .container>div> figure:hover img {
+        transform: none;
+    }
+}</style>
